0f8c82a5c77d246b7b1e432ef6ea328f59ce4c79
Pauline <pauline.design007@gmail.com>
2017-05-26 16:38:08 +1200

    added text to Blog.jsx

--- client/components/Blog.jsx
+++ client/components/Blog.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 const Blog = () => (
   <div className='container'>
    <h1>Pauline's Blog</h1>
+   <p><i>"Have the courage to follow your heart and intuition. They somehow know what you truly want to become"</i></p>
+   <p>Steve Jobs</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
