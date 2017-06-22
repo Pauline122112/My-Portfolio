5cc964dc67c950172d154c91f68332a41731f80b
Pauline <pauline.design007@gmail.com>
2017-06-22 20:26:27 +1200

    added text

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Contact = () => (
   <div className='container'>
    <h1>Contact Pauline</h1>
+   <p>Page is still work in progress</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
