050fdb812ea8b36c26271de1fea10e127b58bb70
Pauline <pauline.design007@gmail.com>
2017-06-22 20:23:52 +1200

    added text

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -5,7 +5,9 @@ const AboutMe = () => (
   <div className='container'>
    <h1>About Pauline</h1>
    <p> Born and raised in the small town of Whangarei, New Zealand</p>
-   <p> Have a degree in Graphic Design</p>
+   <p> Have a degree in Graphic Design</p><br>
+    <p> If you would like more information about me, please don't hesitate to contact me.</p>
+   </br>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
