d75b301320ebbfdce5a9afbbf524cac281016507
Pauline <pauline.design007@gmail.com>
2017-06-22 20:27:21 +1200

    added text

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const AboutMe = () => (
   <div className='container'>
    <h1>About Pauline</h1>
+   <p> Page is still work in progress</p>
    <p> Born and raised in the small town of Whangarei, New Zealand</p>
    <p> Have a degree in Graphic Design</p><br>
     <p> If you would like more information about me, please don't hesitate to contact me.</p>
