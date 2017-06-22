bcb692e74c17eccbcf33f96e7a3acedba3aa3706
Pauline <pauline.design007@gmail.com>
2017-06-22 20:25:06 +1200

    added text

--- client/components/WebDesign.jsx
+++ client/components/WebDesign.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const WebDesign = () => (
   <div className='container'>
    <h1>Web Design</h1>
+   <p>Page is still work in progress</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
