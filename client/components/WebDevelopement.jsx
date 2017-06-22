40d2b12e00f7f7a54240f8e58db7693c61a1f6c4
Pauline <pauline.design007@gmail.com>
2017-06-22 20:27:44 +1200

    added text

--- client/components/WebDevelopement.jsx
+++ client/components/WebDevelopement.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const WebDeveloper = () => (
   <div className='container'>
    <h1>Web Development</h1>
+   <p>Page is still work in progress</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
