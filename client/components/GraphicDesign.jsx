8c1a67825b4d4a7acf49ffe29d2aa64bc3181463
Pauline <pauline.design007@gmail.com>
2017-06-22 20:25:35 +1200

    added text

--- client/components/GraphicDesign.jsx
+++ client/components/GraphicDesign.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const GraphicDesign = () => (
   <div className='container'>
    <h1>Graphic Design</h1>
+   <p>Page is still work in progress</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
