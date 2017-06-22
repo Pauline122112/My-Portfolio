2a1854321f4f53c5654c100fe104b942dea4d447
Pauline <pauline.design007@gmail.com>
2017-06-22 20:56:35 +1200

    deleted text in AboutMe

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -4,10 +4,7 @@ import { Link } from 'react-router-dom'
 const AboutMe = () => (
   <div className='container'>
    <h1>About Pauline</h1>
-   <p> Page is still work in progress</p>
    <p> Born and raised in the small town of Whangarei, New Zealand</p>
-   <p> Have a degree in Graphic Design</p>
-    <p> If you would like more information about me, please don't hesitate to contact me.</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
