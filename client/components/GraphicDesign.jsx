0513f005000a3063ac96a19944c69f70d06f2e2f
Pauline <pauline.design007@gmail.com>
2017-06-23 22:27:03 +1200

    added text

--- client/components/GraphicDesign.jsx
+++ client/components/GraphicDesign.jsx
@@ -5,6 +5,7 @@ const GraphicDesign = () => (
   <div className='container'>
    <h1>Graphic Design</h1>
    <p>Page is still work in progress</p>
+   <p>Please refer to my cv for more information</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
