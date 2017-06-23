6e7a01977643e4ac657c8a27ca6e509232a7bdad
Pauline <pauline.design007@gmail.com>
2017-06-23 22:26:19 +1200

    added text

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -5,6 +5,7 @@ const AboutMe = () => (
   <div className='container'>
    <h1>About Pauline</h1>
    <p> Page is still in progress</p>
+   <p>For further information, follow the link below</p>
 
    <p> Born and raised in the small town of Whangarei, New Zealand</p>
      <Link to='/'>
