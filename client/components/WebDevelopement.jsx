5ab04e96809f25e1c4ac757ff97ca444e2d10b07
Pauline <pauline.design007@gmail.com>
2017-06-23 22:27:35 +1200

    added text

--- client/components/WebDevelopement.jsx
+++ client/components/WebDevelopement.jsx
@@ -5,6 +5,7 @@ const WebDeveloper = () => (
   <div className='container'>
    <h1>Web Development</h1>
    <p>Page is still work in progress</p>
+   <p>Please follow the link below for more information</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
