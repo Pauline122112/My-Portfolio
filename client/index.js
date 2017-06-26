8ca79a9e12683bb634cdd4c198e4fa66e8edc3bc
Pauline <pauline.design007@gmail.com>
2017-06-26 12:17:50 +1200

    made changes to reducers

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 
-import reducers from './reducers'
 import App from './components/App'
 
 const store = createStore(reducers,
