4d008b2b7c123610c7ff74666c0d6c1b02c91e3d
Pauline <pauline.design007@gmail.com>
2017-06-26 12:33:15 +1200

    added back the actions folder

---
+++ client/actions/index.js
@@ -0,0 +1,10 @@
+
+let nextWordId = 0
+
+export const addWord = (word) => {
+  return {
+    type: 'ADD_WORD',
+    id: nextWordId++,
+    word
+  }
+}
--- client/reducers/words.js
+++ client/reducers/words.js
@@ -1,14 +1,15 @@
+
 const words = (state = [], action) => {
   switch (action.type) {
     case 'ADD_WORD':
-     const newState = [
-       ...state,
-       {
-         id: action.id,
-         word: action.word
-       }
-     ]
-     return newState
+      const newState = [
+        ...state,
+        {
+          id: action.id,
+          word: action.word
+        }
+      ]
+      return newState
 
     default:
       return state
