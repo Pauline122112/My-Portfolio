121d18e98792e7fcf900d69bcab14d40e3518fa9
Pauline <pauline.design007@gmail.com>
2017-06-26 12:08:31 +1200

    added text to intro

--- client/components/DefaultPage.jsx
+++ client/components/DefaultPage.jsx
@@ -5,10 +5,14 @@ const DefaultPage = () => (
 
   <div className='container'>
    <h1>Introduction</h1>
-   <p> Page is still work in progress</p>
-   <p> Graduated from Enspiral Dev Academy (Wellington, New Zealand) as a junior web developer(2017)</p>
-   <p> Qualified Graphic Designer specialising in Logo Design</p>
-   <p> Please feel free to take a tour of my awesome Online Portfolio</p>
+   <p>I am a full stack web developer and designer who is looking for work that will utilise my Design and Web Development skills. I have recently been working at Police National Headquarters where I was analysing software and its effectiveness to serve both the police and our clients.  It was here that a flame was reignited around creating processes and solutions that really serve the user. I was also looking for something to stretch me - a new challenge. I wanted to do more than identify improvements in the software I wanted to build and create those solutions. Much to the dismay of my previous employers, I took the leap and found that studying Web Development at Enspiral Dev Academy was the best solution.
+
+Since taking the leap, I have enjoyed working in a team environment and problem solving . I am constantly working on developing my back end development skills. I have found myself gravitating towards front end roles in a team environment mainly because of my strong commercial background in Graphic Design. I also got to see another side of myself by pushing beyond my limits but to also see how others found working with me.
+
+I am a team orientated person who is well spoken, energetic, confident and personable. I enjoy working with people and have had a wide experience working in other countries (including non-english speaking countries)
+
+I’m hoping that with the skills I have obtained will provide me with an interactive learning experience that is challenging and self enhancing.
+ </p>
 
   <div className='AboutMe'>
      <Link to='/AboutMe'>
