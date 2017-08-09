c0aa83913a8e9630e10dfadda0eaa1227204b43f
Pauline <pauline.design007@gmail.com>
2017-08-09 23:59:26 +1200

    changed text

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => (
    <p> Page is still in progress</p>
    <p>For further information, follow the link below</p>
 
-   <p> Born and raised in the small town of Whangarei, New Zealand</p>
+   <p> Born and raised in the small town of Whangarei, New Zealand, I've lived in Auckland</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
