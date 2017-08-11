ad0186e0336fbd2ba1d752e00d678577a0b0bbc3
Pauline <pauline.design007@gmail.com>
2017-08-11 23:39:02 +1200

    added text to about page

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -7,7 +7,8 @@ const AboutMe = () => (
    <p> Page is still in progress</p>
    <p>For further information, follow the link below</p>
 
-   <p> Born and raised in the small town of Whangarei, New Zealand, I've lived in Auckland</p>
+   <p> Born and raised in the small town of Whangarei. Studied at AUT University in Auckland where I first studied Sport and Recreation. From here, my love
+     for art developed and during my second of University, I made a life changing decision and went into Art and Design.</p>
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
