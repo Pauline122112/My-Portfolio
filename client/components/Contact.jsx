47b653a1a5985cbdb0d86988a144ddf1284f87b3
Pauline <pauline.design007@gmail.com>
2017-08-11 23:43:15 +1200

    adding contact form

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -3,8 +3,20 @@ import { Link } from 'react-router-dom'
 
 const Contact = () => (
   <div className='container'>
-   <h1>Contact Pauline</h1>
-   <p>Page is still work in progress</p>
+    <form action="/action_page.php">
+      <label for="fname">First Name</label>
+         <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+         <label for="lname">Last Name</label>
+         <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+         <label for="country">Country</label>
+         <select id="country" name="country">
+           <option value="australia">Australia</option>
+           <option value="canada">Canada</option>
+           <option value="usa">USA</option>
+         </select>
+
      <Link to='/'>
            <img src='' alt='Home' />
      </Link>
